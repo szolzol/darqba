9e9849b33d5cb8a25d99474445997e86d9f6c83c
szolzol <zoltan.szoleczki@cemsmail.org>
2023-08-04 14:45:27 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header class="bg-white sticky top-0 z-50 flex justify-between items-center space-x-1 border-b p-4 shadow-md">
+      <a href="" class="text-3xl font-mono">Darqba</a>
+    </header>
+
+    <!-- navbar -->
+    <!-- home hero -->
+    <div
+      class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://images.unsplash.com/photo-1534577403868-27b805ca4b9c?fit=fillmax&fm=jpg&ixid=MnwzNTY3MHwwfDF8YWxsfHx8fHx8fHx8MTY4Mjg3OTQzOA&ixlib=rb-4.0.3&q=75')]">
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center.w-1/2">
+          <h1 class="z-40 text-8xl font-mono text-white mb-10">Find your story</h1>
+          <button type="button"
+            class="py-2 px-4 max-w-md flex justify-center items-center bg-black hover:bg-red-900 focus:ring-red-800 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
+            <svg width="20" height="20" fill="currentColor" class="mr-2" viewBox="0 0 1792 1792"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z">
+              </path>
+            </svg>
+            Sign in with Google
+          </button>
+
+        </div>
+
+      </div>
+
+    </div>
+    <!-- home hero -->
+
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans]
+      },
+    },
+  },
+  variants: {
+    extend: {}
+  },
+};
