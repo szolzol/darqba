427569edc360b229157637fa27e03477216241a1
szolzol <zoltan.szoleczki@cemsmail.org>
2023-08-10 19:41:05 +0200

    Pages trial 2

--- predeploy-functions.bat
+++
@@ -1,5 +0,0 @@
-rmdir functions\nuxt /s /q
-mkdir functions\nuxt
-xcopy .nuxt functions\nuxt /e
-del functions\nuxt.config.js
-xcopy nuxt.config.js functions
--- predeploy-hosting.bat
+++
@@ -1,5 +0,0 @@
-rmdir public /s /q
-mkdir public
-mkdir public\_nuxt
-xcopy .nuxt\dist\client public\_nuxt /e
-xcopy static public /e
