1305962d24f800c7d28de72762244488b305e9a3
szolzol <zoltan.szoleczki@cemsmail.org>
2023-08-11 14:13:38 +0200

    Vercel Init 3

--- .output/server/chunks/app/_nuxt/IconCSS-styles.3a98f00b.mjs
+++
@@ -1,6 +0,0 @@
-const IconCSS_vue_vue_type_style_index_0_scoped_45f00bb8_lang = "span[data-v-45f00bb8]{background-color:currentColor;display:inline-block;-webkit-mask-image:var(--fb442de6);mask-image:var(--fb442de6);-webkit-mask-repeat:no-repeat;mask-repeat:no-repeat;-webkit-mask-size:100% 100%;mask-size:100% 100%;vertical-align:middle}";
-
-const IconCSSStyles_3a98f00b = [IconCSS_vue_vue_type_style_index_0_scoped_45f00bb8_lang, IconCSS_vue_vue_type_style_index_0_scoped_45f00bb8_lang];
-
-export { IconCSSStyles_3a98f00b as default };
-//# sourceMappingURL=IconCSS-styles.3a98f00b.mjs.map
